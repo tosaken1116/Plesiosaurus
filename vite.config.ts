cbee7ca0e8fbcd8bd67e4581ae8f304de91f22cc
tosaken1116 <nagato20021122@gmail.com>
2023-11-15 20:28:00 +0900

    feat: add vanilla extract

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,18 @@
-import { defineConfig } from "vitest/config";
+import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vitest/config";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     test: {
         globals: true,
         environment: "jsdom",
-        include: ["src/components/**/*.test.@(tsx|ts)"],
+        include: ["src/**/*.test.@(tsx|ts)"],
     },
-    plugins: [react()],
+    css: {
+        modules: {
+            localsConvention: "camelCase",
+        },
+    },
+    plugins: [react(), vanillaExtractPlugin()],
 });
