4630597b790b499ec83ddc4efd076e62937424bb
saku-1101 <sakupi-1101@outlook.jp>
2023-11-29 16:52:00 +0900

    chore: wip

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,29 @@
 import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vitest/config";
+import pkg from './package.json';
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    build: {
+        lib: {
+          // Could also be a dictionary or array of multiple entry points
+          entry: path.resolve(__dirname, 'src/index.ts'),
+        },
+        rollupOptions: {
+            external: Object.keys(pkg.peerDependencies), // files that should not be bundled
+            output: {
+              interop: 'auto', // the default mode of "default" mimics NodeJS behavior and is different from TypeScript esModuleInterop
+              exports: 'named', // 'default' can cause issues when generating CommonJS output that is meant to be interchangeable with ESM output
+              preserveModules: true,
+              preserveModulesRoot: 'src',
+              entryFileNames: ({ name: fileName }) => {
+                return `${fileName}.js`;
+              }
+            }
+          }
+      },
     test: {
         globals: true,
         environment: "jsdom",
