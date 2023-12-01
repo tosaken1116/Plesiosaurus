a48a69e90d1d4df5ed9d1902f470211e2ad671a9
saku-1101 <sakupi-1101@outlook.jp>
2023-12-01 21:31:07 +0900

    chore: export方法の修正など

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,35 +1,34 @@
 /** @format */
+import { forwardRef } from 'react'
 
-import { forwardRef } from "react";
+import { clsx } from 'clsx'
 
-import { clsx } from "clsx";
-
-import { states } from "./index.css";
-import { radius } from "./index.css";
+import { states } from './index.css'
+import { radius } from './index.css'
 
 export type ButtonProps = {
-    className?: string;
-    state?: keyof typeof states;
-    roundness?: keyof typeof radius;
-    ref?: React.Ref<HTMLButtonElement>;
-    children?: React.ReactNode;
-} & React.ButtonHTMLAttributes<HTMLButtonElement>;
+  className?: string
+  state?: keyof typeof states
+  roundness?: keyof typeof radius
+  ref?: React.Ref<HTMLButtonElement>
+  children?: React.ReactNode
+} & React.ButtonHTMLAttributes<HTMLButtonElement>
 
 export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
-    ({
-        className,
-        state = "primary",
-        roundness = "md",
-        ref,
-        children,
-        ...props
-    }: ButtonProps) => (
-        <button
-            className={clsx(states[state], radius[roundness], className)}
-            ref={ref}
-            {...props}
-        >
-            {children}
-        </button>
-    )
-);
+  ({
+    className,
+    state = 'primary',
+    roundness = 'md',
+    ref,
+    children,
+    ...props
+  }: ButtonProps) => (
+    <button
+      className={clsx(states[state], radius[roundness], className)}
+      ref={ref}
+      {...props}
+    >
+      {children}
+    </button>
+  ),
+)
