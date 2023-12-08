e545294a153ce8f45119f6836521c64448494191
saku-1101 <sakupi-1101@outlook.jp>
2023-12-08 23:19:48 +0900

    test: add test of Typography comp

--- src/components/Typography/index.test.tsx
+++ src/components/Typography/index.test.tsx
@@ -1,14 +1,19 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import { describe, expect, test } from 'vitest'
 
-import "@testing-library/jest-dom";
-import { Typography } from ".";
+import { Typography } from '.'
 
-describe("ui/Typography", () => {
-  it("title is exist", () => {
-    render(<Typography />);
+describe('ui/Typography', () => {
+  test('If Heading 1 Component is correctly rendered.', () => {
+    render(
+      <Typography component='h1' variant='h1'>
+        This is H1.
+      </Typography>,
+    )
 
-    const title = screen.getByText(/this is ui of Typography/);
+    const title = screen.getByRole('heading')
 
-    expect(title).toBeInTheDocument();
-  });
-});
+    expect(title).toHaveTextContent('This is H1.')
+  })
+})
