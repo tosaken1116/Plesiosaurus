9a3bb375aa3bed100deeb89bf1cf9fb8d5c69fd6
saku-1101 <sakupi-1101@outlook.jp>
2023-12-08 22:50:35 +0900

    feat: 🌟 Typography Component (without animation)

---
+++ src/font.css.ts
@@ -0,0 +1,17 @@
+import { styleVariants } from '@vanilla-extract/css'
+
+import { vars } from './global.css'
+
+export const fontStyles = styleVariants({
+  h1: vars.typography.h1,
+  h2: vars.typography.h2,
+  h3: vars.typography.h3,
+  h4: vars.typography.h4,
+  h5: vars.typography.h5,
+  h6: vars.typography.h6,
+  p: vars.typography.p,
+  'body-large': vars.typography['body-large'],
+  'body-medium': vars.typography['body-medium'],
+  'body-small': vars.typography['body-small'],
+  'backquote': vars.typography['backquote'],
+})
