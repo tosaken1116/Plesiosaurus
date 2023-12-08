62c8a96dc57fd5a8af664d41a886d415ef540193
saku-1101 <sakupi-1101@outlook.jp>
2023-12-08 22:50:35 +0900

    feat: 🌟 Typography Component (without animation)

--- src/components/Typography/index.tsx
+++ src/components/Typography/index.tsx
@@ -1,10 +1,19 @@
 import 'modern-normalize/modern-normalize.css'
 import '../../reset.css'
+import { forwardRef } from 'react'
+
 import { clsx } from 'clsx'
 
+import { fontStyles } from '../../font.css'
+
 export type TypographyProps = {
+  component: React.ElementType
+  variant: keyof typeof fontStyles
+  children: React.ReactNode
   className?: string
-}
+} & React.HTMLAttributes<HTMLHeadingElement> &
+  React.HTMLAttributes<HTMLParagraphElement> &
+  React.HTMLAttributes<HTMLSpanElement>
 
 /**
  * @name Typography component
@@ -13,6 +22,16 @@ export type TypographyProps = {
  * @returns {JSX.Element}
  * @example <Typography a={1} />
  */
-export const Typography = ({ className }: TypographyProps): JSX.Element => (
-  <p className={clsx(className)} />
+const Typography = forwardRef<HTMLHeadingElement, TypographyProps>(
+  ({ component, variant, children, className, ...props }: TypographyProps, ref) => {
+    const Component = component
+
+    return (
+      <Component className={clsx(fontStyles[variant], className)} ref={ref} {...props}>
+        {children}
+      </Component>
+    )
+  },
 )
+
+export { Typography }
