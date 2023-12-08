49a382ddc6bdba54380cb08bea5a1b9e03730e2f
saku-1101 <sakupi-1101@outlook.jp>
2023-12-08 21:45:19 +0900

    chore: init Typography Component

--- _templates/generator/ui/prompt.js
+++ _templates/generator/ui/prompt.js
@@ -1,28 +1,28 @@
-const fs = require("fs");
-const path = "./src/components/index.ts";
+const fs = require('fs')
+const path = './src/components/index.ts'
 
 module.exports = {
-    prompt: ({ prompter }) => {
-        return prompter
-            .prompt([
-                {
-                    type: "input",
-                    name: "name",
-                    message: "component name",
-                },
-            ])
-            .then(({ name }) => {
-                const contentToAdd = `\nexport * from './${name}';\n`;
-                fs.appendFile(path, contentToAdd, "utf8", (err) => {
-                    if (err) {
-                        console.error("ファイルの書き込みに失敗しました:", err);
-                    } else {
-                        console.log("ファイルにテキストを追加しました。");
-                    }
-                });
-                return {
-                    name,
-                };
-            });
-    },
-};
+  prompt: ({ prompter }) => {
+    return prompter
+      .prompt([
+        {
+          type: 'input',
+          name: 'name',
+          message: 'component name',
+        },
+      ])
+      .then(({ name }) => {
+        const contentToAdd = `\nexport * from './${name}';\n`
+        fs.appendFile(path, contentToAdd, 'utf8', (err) => {
+          if (err) {
+            console.error('ファイルの書き込みに失敗しました:', err)
+          } else {
+            console.log('ファイルにテキストを追加しました。')
+          }
+        })
+        return {
+          name,
+        }
+      })
+  },
+}
