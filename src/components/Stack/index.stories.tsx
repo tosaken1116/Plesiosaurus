546d2c3998680b42febbf581f5cb8b4f7579905f
saku-1101 <sakupi-1101@outlook.jp>
2023-12-09 21:19:29 +0900

    feat: 🌟 Stack Component

--- src/components/Stack/index.stories.tsx
+++ src/components/Stack/index.stories.tsx
@@ -1,6 +1,6 @@
-import { Stack } from '.';
+import { Stack } from '.'
 
-import type { Meta, StoryObj } from '@storybook/react';
+import type { Meta, StoryObj } from '@storybook/react'
 
 const meta: Meta<typeof Stack> = {
   component: Stack,
@@ -8,12 +8,23 @@ const meta: Meta<typeof Stack> = {
     layout: 'centered',
   },
   tags: ['autodocs'],
-};
+}
 
-export default meta;
+export default meta
 
-type Story = StoryObj<typeof Stack>;
+type Story = StoryObj<typeof Stack>
 
 export const Default: Story = {
-  args: {},
-};
+  args: {
+    children: [
+      <div>This is Stack</div>,
+      <div>This is Stack</div>,
+      <div>This is Stack</div>,
+    ],
+    direction: { sm: 'row', md: 'row' },
+    spacing: { sm: 0, md: 2 },
+    alignItems: { sm: 'center', md: 'center' },
+    justifyContent: { sm: 'center', md: 'center' },
+    divider: <p>|</p>,
+  },
+}
--- src/components/Stack/index.tsx
+++ src/components/Stack/index.tsx
@@ -1,9 +1,31 @@
 import 'modern-normalize/modern-normalize.css'
 import '../../reset.css'
+import { forwardRef } from 'react'
+import type { HTMLAttributes } from 'react'
 
+import clsx from 'clsx'
+
+import { sprinkles } from '../../layout.css'
+
+import { joinChildren } from './libs/joinChildren'
+
+import type { Sprinkles } from '../../layout.css'
+
+type StackDirection = Sprinkles['flexDirection']
+type Spacing = Sprinkles['gap']
+type JustifyContent = Sprinkles['justifyContent']
+type AlignItems = Sprinkles['alignItems']
 export type StackProps = {
+  component?: React.ElementType
+  direction?: StackDirection
+  divider?: React.ReactNode
+  spacing?: Spacing
+  justifyContent?: JustifyContent
+  alignItems?: AlignItems
   className?: string
-}
+  children?: React.ReactNode[] | React.ReactNode
+} & React.HTMLProps<HTMLElement> &
+  HTMLAttributes<HTMLElement>
 
 /**
  * @name Stack component
@@ -12,8 +34,48 @@ export type StackProps = {
  * @returns {JSX.Element}
  * @example <Stack a={1} />
  */
-export const Stack = ({ className }: StackProps): JSX.Element => (
-  <div className={className} data-testId='stack'>
-    Stack
-  </div>
+export const Stack = forwardRef<HTMLElement, StackProps>(
+  ({
+    component = 'div',
+    direction,
+    divider,
+    spacing,
+    justifyContent,
+    alignItems,
+    className,
+    ref,
+    children,
+    ...props
+  }: StackProps): JSX.Element => {
+    const Component = component
+
+    return (
+      <Component
+        className={clsx(
+          sprinkles({
+            display: 'flex',
+            flexDirection: direction,
+            justifyContent: justifyContent,
+            alignItems: alignItems,
+            gap: spacing,
+          }),
+          className,
+        )}
+        data-testId='stack'
+        ref={ref}
+        {...props}
+      >
+        {divider
+          ? joinChildren(children, (index: string) => (
+              <DividerWrapper key={index}>{divider}</DividerWrapper>
+            ))
+          : children}
+      </Component>
+    )
+  },
 )
+
+type DividerWrapperProps = {
+  children: React.ReactNode
+}
+const DividerWrapper = ({ children }: DividerWrapperProps): JSX.Element => <>{children}</>
