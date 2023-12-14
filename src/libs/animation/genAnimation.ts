6be2dae2691ce40fc7d1ed734e66ce26e26dab46
saku-1101 <sakupi-1101@outlook.jp>
2023-12-14 21:40:19 +0900

    refactor: アニメーションIFの抽象化

--- src/libs/animation/genAnimation.ts
+++ src/libs/animation/genAnimation.ts
@@ -4,21 +4,24 @@ import { AnimationFactory } from './variant/AnimationFactory'
 
 import type { AnimationObject, AnimationProps } from './variant/AnimationFactory'
 
-const animateObject = (props?: AnimationObject, isHover = false): string => {
+const animateObject = <T>(props?: AnimationObject<T>, isHover = false): string => {
   if (!props) {
     return ''
   }
+
   const { animation, option } = props
 
   const genFunction = AnimationFactory[animation]
+
   if (genFunction) {
+    // @ts-expect-error : Generic typeはこの時点では不明だが、実行時には確定しているので問題ない
     return genFunction(option, isHover)
   } else {
     throw new Error('Invalid animation type')
   }
 }
 
-export const genAnimation = (props: AnimationProps): string => {
+export const resolveAnimation = <T>(props: AnimationProps<T>): string => {
   const { in: inAnimate, hover } = props
   if (!inAnimate && !hover) {
     throw new Error('Missing animation props')
--- src/libs/animation/variant/AnimationFactory.ts
+++ src/libs/animation/variant/AnimationFactory.ts
@@ -1,19 +1,24 @@
+import { genBackGroundColorFadeAnimation } from './BackGroundColorFade'
 import { genSlideAnimation } from './Slide'
 
+import type { BackGroundColorFadeProps } from './BackGroundColorFade'
 import type { SlideProps } from './Slide'
+import type { AnimationBaseProps } from './type'
 
-export type AnimationType = SlideProps
+export type AnimationType<T> = AnimationBaseProps &
+  (T extends 'slide' ? SlideProps : BackGroundColorFadeProps)
 
 export const AnimationFactory = {
   slide: genSlideAnimation,
+  backGroundColorFade: genBackGroundColorFadeAnimation,
 }
 
-export type AnimationObject = {
+export type AnimationObject<T> = {
   animation: keyof typeof AnimationFactory
-  option?: AnimationType
+  option?: AnimationType<T>
 }
 
-export type AnimationProps = {
-  in?: AnimationObject
-  hover?: AnimationObject
+export type AnimationProps<T> = {
+  in?: AnimationObject<T>
+  hover?: AnimationObject<T>
 }
