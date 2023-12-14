340b5ff389b5a9fe6e1f77ec303c9cb7806c70b4
saku-1101 <sakupi-1101@outlook.jp>
2023-12-14 21:51:32 +0900

    style: textcolorアニメーションを追加

---
+++ src/libs/animation/variant/TextColor/type.ts
@@ -0,0 +1,9 @@
+export type TextColorAnimationProps = {
+  afterColor?: 'primary' | 'secondary' | 'error'
+  // | 'success'
+  // | 'warning'
+  // | 'info'
+  // | 'light'
+  // | 'dark',
+  duration?: '0s' | '0.3s' | '0.5s' | '0.8s'
+}
