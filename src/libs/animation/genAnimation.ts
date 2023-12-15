f5209276f891f1089cb7ead94967f1edddc02f6e
tosaken1116 <nagato20021122@gmail.com>
2023-12-15 14:04:38 +0900

    feat: animation only hover or  in

--- src/libs/animation/genAnimation.ts
+++ src/libs/animation/genAnimation.ts
@@ -1,5 +1,3 @@
-import clsx from 'clsx'
-
 import { AnimationFactory } from './variant/AnimationFactory'
 
 import type { AnimationObject, AnimationProps } from './variant/AnimationFactory'
@@ -29,12 +27,10 @@ export const genAnimation = (
 ): { className: string; style: object } => {
   const { in: inAnimate, hover } = props
   if (!inAnimate && !hover) {
-    throw new Error('Missing animation props')
+    return { className: '', style: {} }
   }
-  const { className, style } = animateObject(inAnimate)
-  const { className: hoverClassName, style: hoverStyle } = animateObject(hover, true)
-  return {
-    className: clsx(className, hoverClassName),
-    style: { ...style, ...hoverStyle },
+  if (inAnimate) {
+    return animateObject(inAnimate)
   }
+  return animateObject(hover, true)
 }
