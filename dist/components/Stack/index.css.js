8e0efdb0af725007186fd82f0fc9a1b5ccd20f79
saku-1101 <sakupi-1101@outlook.jp>
2023-12-17 02:12:52 +0900

    fix: exclude .stoires out of bundling

--- dist/components/Stack/index.css.js
+++ dist/components/Stack/index.css.js
@@ -1,4 +1,4 @@
-import "../../index.css";
+import "../../index2.css";
 import { c as a } from "../../createRuntimeFn-f8e161c6.esm-o8UowZGi.mjs";
 var r = a({ defaultClassName: "_11dl3cx0", variantClassNames: { flexDirection: { row: "_11dl3cx1", column: "_11dl3cx2" } }, defaultVariants: {}, compoundVariants: [] });
 export {
