7809e7dc60e0bca800b1d3e1ec8b64f00dca0946
tosaken1116 <nagato20021122@gmail.com>
2023-12-16 12:49:57 +0900

    feat: add Badge Component

--- src/components/Badge/index.stories.tsx
+++ src/components/Badge/index.stories.tsx
@@ -1,6 +1,8 @@
-import { Badge } from '.';
+import { Heart } from 'lucide-react'
 
-import type { Meta, StoryObj } from '@storybook/react';
+import { Badge } from '.'
+
+import type { Meta, StoryObj } from '@storybook/react'
 
 const meta: Meta<typeof Badge> = {
   component: Badge,
@@ -8,12 +10,46 @@ const meta: Meta<typeof Badge> = {
     layout: 'centered',
   },
   tags: ['autodocs'],
-};
+}
 
-export default meta;
+export default meta
 
-type Story = StoryObj<typeof Badge>;
+type Story = StoryObj<typeof Badge>
 
 export const Default: Story = {
-  args: {},
-};
+  args: {
+    children: (
+      <div style={{ padding: '2rem', backgroundColor: 'gray', borderRadius: '8px' }}>
+        Badge
+      </div>
+    ),
+    badgeComponent: '1',
+  },
+}
+
+export const LargeBadge: Story = {
+  args: {
+    children: (
+      <div style={{ padding: '2rem', backgroundColor: 'gray', borderRadius: '8px' }}>
+        Badge
+      </div>
+    ),
+    badgeComponent: '1000000',
+  },
+}
+
+export const CustomBadge: Story = {
+  args: {
+    children: (
+      <div style={{ padding: '2rem', backgroundColor: 'gray', borderRadius: '8px' }}>
+        Badge
+      </div>
+    ),
+    badgeComponent: (
+      <span style={{ display: 'flex', alignItems: 'center', padding: '0.5rem 0' }}>
+        <Heart />
+        1000
+      </span>
+    ),
+  },
+}
--- src/components/Badge/index.tsx
+++ src/components/Badge/index.tsx
@@ -1,13 +1,32 @@
 import 'modern-normalize/modern-normalize.css'
 import '../../reset.css'
+import type { ReactNode } from 'react'
 
-export type BadgeProps = { className?: string }
+import clsx from 'clsx'
+
+import { badgeStyle, containerStyle } from './index.css'
+
+export type BadgeProps = {
+  children: ReactNode
+  badgeComponent: ReactNode
+  className?: string
+}
 
 /**
  * @name Badge component
  * Badgeを表示するコンポーネント
- * @param {number} a - props a
+ * @param {ReactNOde} children - main component to display
+ * @param {ReactNode} badgeComponent - Badge component to display
  * @returns {JSX.Element}
  * @example <Badge a={1} />
  */
-export const Badge = ({ className }: BadgeProps): JSX.Element => <>{className}</>
+export const Badge = ({
+  children,
+  badgeComponent,
+  className,
+}: BadgeProps): JSX.Element => (
+  <span className={containerStyle}>
+    <span className={clsx(badgeStyle, className)}>{badgeComponent}</span>
+    {children}
+  </span>
+)
