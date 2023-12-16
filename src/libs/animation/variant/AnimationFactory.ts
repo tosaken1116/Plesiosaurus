114dee24cf8840a1d81fad7587d1e26aba4dff1f
tosaken1116 <nagato20021122@gmail.com>
2023-12-16 14:23:41 +0900

    feat: add squeeze animation

--- src/libs/animation/variant/AnimationFactory.ts
+++ src/libs/animation/variant/AnimationFactory.ts
@@ -1,9 +1,11 @@
 import { genBackGroundColorFadeAnimation } from './BackGroundColorFade'
 import { genSlideAnimation } from './Slide'
+import { genSqueezeAnimation } from './Squeeze'
 import { genTextColorAnimation } from './TextColor'
 
 import type { BackGroundColorFadeProps } from './BackGroundColorFade'
 import type { SlideProps } from './Slide'
+import type { SqueezeProps } from './Squeeze'
 import type { TextColorProps } from './TextColor'
 import type { GeneratedAnimationValue } from './type'
 
@@ -12,7 +14,11 @@ export type AnimationProps = {
   hover?: AnimationObject
 }
 
-export type AnimationObject = SlideProps | BackGroundColorFadeProps | TextColorProps
+export type AnimationObject =
+  | SlideProps
+  | BackGroundColorFadeProps
+  | TextColorProps
+  | SqueezeProps
 
 export const AnimationFactory: Record<
   AnimationObject['key'],
@@ -21,4 +27,5 @@ export const AnimationFactory: Record<
   slide: genSlideAnimation,
   bgColorFade: genBackGroundColorFadeAnimation,
   textColor: genTextColorAnimation,
+  squeeze: genSqueezeAnimation,
 } as const
---
+++ src/libs/animation/variant/Squeeze/type.ts
@@ -0,0 +1,10 @@
+import type { AnimationBaseProps } from '../type'
+
+export type OptionProps = {
+  axis?: 'x' | 'y'
+}
+
+export type SqueezeProps = {
+  key: 'squeeze'
+  option: OptionProps & AnimationBaseProps
+}
