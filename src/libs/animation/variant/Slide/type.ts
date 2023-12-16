c1980c15991eb9497eff37b8165a875198ef3178
saku-1101 <sakupi-1101@outlook.jp>
2023-12-16 16:40:12 +0900

    refactor: refactor Button component

--- src/libs/animation/variant/Slide/type.ts
+++ src/libs/animation/variant/Slide/type.ts
@@ -1,8 +1,10 @@
-export type SlideAnimationProps = {
-  duration?: '1s' | '2s' | '3s'
-  delay?: '1s' | '2s' | '3s'
+import type { AnimationBaseProps } from '../type'
+
+export type OptionProps = {
   direction?: 'left' | 'right' | 'top' | 'bottom'
-  distance?: number
-  easing?: string
-  visible?: boolean
+}
+
+export type SlideProps = {
+  key: 'slide'
+  option: OptionProps & AnimationBaseProps
 }
