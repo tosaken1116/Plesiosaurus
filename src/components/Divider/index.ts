08bfd02c56fd2162854771a349c46571e52a8e94
tosaken1116 <nagato20021122@gmail.com>
2023-12-16 21:50:58 +0900

    chore: remove Divider Stack index

--- src/components/Divider/index.ts
+++
@@ -1,7 +0,0 @@
-import { Divider } from "./index.tsx";
-
-import type { DividerProps } from "./index.tsx";
-
-export { Divider };
-
-export type { DividerProps };
--- src/components/Stack/index.ts
+++
@@ -1,7 +0,0 @@
-import { Stack } from "./index.tsx";
-
-import type { StackProps } from "./index.tsx";
-
-export { Stack };
-
-export type { StackProps };
