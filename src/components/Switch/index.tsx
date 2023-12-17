fbd2857d77fdc536cec9af78cb341044469b66c6
uuronn <uuronprogram@gmail.com>
2023-12-17 11:11:07 +0900

    chore: テストコードを削除

--- src/components/Switch/index.tsx
+++ src/components/Switch/index.tsx
@@ -2,7 +2,15 @@
 import 'modern-normalize/modern-normalize.css'
 import '../../reset.css'
 
+import { forwardRef } from 'react'
+
+import clsx from 'clsx'
+
 import { toggleButtonInput, toggleButtonLabel } from './index.css'
+
+export type SwitchProps = {
+  className?: string
+}
 
 /**
  * @name Switch component
@@ -11,10 +19,9 @@ import { toggleButtonInput, toggleButtonLabel } from './index.css'
  * @returns {JSX.Element}
  * @example <Switch a={1} />
  */
-export const Switch = (): JSX.Element => (
-  <label htmlFor='switch' className={toggleButtonLabel}>
-    <input type='checkbox' className={toggleButtonInput} id='switch' />
-  </label>
-)
 
-// A form label must be associated with a control.
+export const Switch = forwardRef<HTMLInputElement, SwitchProps>(({ className }, ref) => (
+  <label className={clsx(toggleButtonLabel, className)} htmlFor='switch'>
+    <input type='checkbox' className={toggleButtonInput} ref={ref} id='switch' />
+  </label>
+))
